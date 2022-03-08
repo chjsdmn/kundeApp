3723c05a0dd8576cdbee371952617907caea7a4a
chjsdmn <chjsdmn@hotmail.com>
2022-03-08 12:05:09 +0100

    simplified code for getting value of forekort

--- src/main/java/com/example/kundeapp/model/Kunde.java
+++ src/main/java/com/example/kundeapp/model/Kunde.java
@@ -3,10 +3,30 @@ package com.example.kundeapp.model;
 public class Kunde {
     private String navn;
     private String address;
+    private String bilmerke;
+    private boolean forekort;
 
-    public Kunde(String navn, String address) {
+    public Kunde(String navn, String address, String bilmerke, boolean forekort) {
         this.navn = navn;
         this.address = address;
+        this.bilmerke = bilmerke;
+        this.forekort = forekort;
+    }
+
+    public String getBilmerke() {
+        return bilmerke;
+    }
+
+    public void setBilmerke(String bilmerke) {
+        this.bilmerke = bilmerke;
+    }
+
+    public boolean isForekort() {
+        return forekort;
+    }
+
+    public void setForekort(boolean forekort) {
+        this.forekort = forekort;
     }
 
     public String getNavn() {
--- src/main/resources/static/lagre.js
+++ src/main/resources/static/lagre.js
@@ -2,34 +2,48 @@ $(() => {
     $("#registre").click(() => {
         const navn = $("#navn");
         const address = $("#address");
+        const bilmerke = $("input[name=bilvalg]:checked");
+        const forekort = /*$("input[name=forekort]:checked");*/ $("#forekort").prop("checked")
+        //$("#forekort").[0].checked JavaScript方法, 返回true/false
+        //$("#forekort").prop("checked") jQuery方法, 返回true/false
+        /*let forekort;
+        if(getForekort.val()) {
+            forekort = true;
+        }
+        else {
+            forekort = false;
+        }*/
         const kunde = {
             //在这里取值而不是上面，原因貌似是保证等到的object的值正确?...
             navn: navn.val(),
-            address: address.val()
+            address: address.val(),
+            bilmerke: bilmerke.val(),
+            forekort: forekort
         }
 
-        //如果两个input都填了，再执行下面的，否则不要劳烦服务器去储存data！
+        //如果input都填了，再执行下面的，否则不要劳烦服务器去储存data！
         if(inputVal(kunde)){
             $.post("/api", kunde, () => {
-                fetchKunde();
+                //点击按钮后直接跳转到index.html页面
+                //不要在这里叫fetchKunde()，因为这个页面不需要！！！
+                window.location.href = "/";
             })
-            navn.val("");
-            address.val("");
         }
         else {
             console.log("Wrong input");
         }
 
 
-        //点击按钮后直接跳转到index.html页面
-        window.location.href = "index.html";
+
     })
 })
 
-//检验是否都输入了
+//检验是否都输入了, 参数是一个kunde对象！！
 const inputVal = kunde => {
     if (kunde.navn === "") return false;
-    //可以直接return最后一项
-    else return kunde.address !== "";
+    else if (kunde.address === "") return false;
+    else return kunde.bilmerke !== "";
+    //最后一项直接return!!!
+    //else return kunde.forekort !== "";
 
 }
